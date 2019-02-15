ef5e6c22dbd05775b102c3c24f53aedd39aff84c
ilgiz <4ilgiz@gmail.com>
2019-02-15 22:45:52 +0300

    array

---
+++ src/arrays/Main.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        System.out.println(arr1.length);
+
+        arr1 = new int[25];
+        System.out.println(arr1.length);
+    }
+}
